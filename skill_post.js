5485cc2d11f6e06fa427ae7198e66d58ecce2eac
dp2genius <dp2genius@gmail.com>
2023-10-16 12:55:57 -0700

    Add main fetch

---
+++ skill_post.js
@@ -0,0 +1,29 @@
+fetch("https://www.upwork.com/freelancers/api/v2/profile/me/skills", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "Bearer oauth2v2_7505610a0a70f733da1eb536d8012cd2",
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Chromium\";v=\"118\", \"Google Chrome\";v=\"118\", \"Not=A?Brand\";v=\"99\"",
+    "sec-ch-ua-full-version-list": "\"Chromium\";v=\"118.0.5993.71\", \"Google Chrome\";v=\"118.0.5993.71\", \"Not=A?Brand\";v=\"99.0.0.0\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-viewport-width": "1182",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "vnd-eo-parent-span-id": "ba6b1795-3a90-4bab-9d01-a95189312692",
+    "vnd-eo-span-id": "e4d927c5-a600-4081-9a64-53693a5697c5",
+    "vnd-eo-trace-id": "817193e72dd7030b-GRU",
+    "x-odesk-csrf-token": "a2064f3b7cf3f5894492f847dd0d17e9",
+    "x-odesk-user-agent": "oDesk LM",
+    "x-requested-with": "XMLHttpRequest",
+    "x-upwork-accept-language": "en-US"
+  },
+  "referrer": "https://www.upwork.com/freelancers/~017086dc3e5e8aff57/modal-skills-edit?pageTitle=Edit%20skills&_modalInfo=%5B%7B%22navType%22%3A%22modal%22,%22title%22%3A%22Edit%20skills%22,%22modalId%22%3A%221697483551341%22%7D%5D",
+  "referrerPolicy": "origin-when-cross-origin",
+  "body": "{\"skills\":[{\"name\":\"Web Application\",\"prettyName\":\"Web Application\",\"active\":true,\"uid\":\"1110580737240190976\",\"rank\":1,\"description\":null,\"externalUrl\":\"https://en.wikipedia.org/wiki/Web_application\",\"certificates\":null,\"ontologySkill\":{\"uid\":\"1110580737240190976\",\"ontologyId\":\"upwork:webapplication\",\"prefLabel\":\"Web Application\",\"altLabel\":[\"Web Apps\"],\"entityStatus\":\"ACTIVE\",\"scopeNote\":null,\"externalLink\":null,\"definition\":null,\"comment\":null,\"attributeLevel\":null,\"allowMultipleClient\":false,\"allowMultipleFreelancer\":false,\"allowOther\":false,\"clientQuestion\":null,\"clientTip\":null,\"freelancerQuestion\":null,\"freelancerTip\":null,\"isAdvancedClient\":false,\"isClient\":false,\"isFreelancer\":false,\"isRequiredClient\":false,\"isRequiredFreelancer\":false,\"otherClientLabel\":null,\"otherFreelancerLabel\":null,\"presentationMode\":null,\"priority\":0,\"legacySkillNid\":\"web-application\",\"legacySkillUid\":\"1108339179743744000\"}},{\"name\":\"Artificial Intelligence\",\"prettyName\":\"Artificial Intelligence\",\"active\":true,\"uid\":\"1031626716094119936\",\"rank\":2,\"description\":\"Artificial intelligence (AI) is the intelligence of machines and the branch of computer science that aims to create it.\",\"externalUrl\":\"https://en.wikipedia.org/wiki/Artificial_intelligence\",\"certificates\":null,\"ontologySkill\":{\"uid\":\"1031626716094119936\",\"ontologyId\":\"upwork:artificialintelligence\",\"prefLabel\":\"Artificial Intelligence\",\"altLabel\":null,\"entityStatus\":\"ACTIVE\",\"scopeNote\":null,\"externalLink\":null,\"definition\":null,\"comment\":null,\"attributeLevel\":null,\"allowMultipleClient\":false,\"allowMultipleFreelancer\":false,\"allowOther\":false,\"clientQuestion\":null,\"clientTip\":null,\"freelancerQuestion\":null,\"freelancerTip\":null,\"isAdvancedClient\":false,\"isClient\":false,\"isFreelancer\":false,\"isRequiredClient\":false,\"isRequiredFreelancer\":false,\"otherClientLabel\":null,\"otherFreelancerLabel\":null,\"presentationMode\":null,\"priority\":0,\"legacySkillNid\":\"artificial-intelligence\",\"legacySkillUid\":\"475721704314667021\"},\"slug\":\"ai-developers\"}]}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
